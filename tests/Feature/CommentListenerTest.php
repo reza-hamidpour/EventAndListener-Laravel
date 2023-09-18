3ce82898f604d13ba3612a935e3173c6e78dd860
Reza Hamidpour <hamidpourr@gmail.com>
2023-09-17 23:38:47 -0700

    Implementing Test for 2 Comment and a new User.

--- tests/Feature/CommentListenerTest.php
+++ tests/Feature/CommentListenerTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use App\Events\CommentWritten;
 use App\Listeners\CommentListener;
+use App\Models\Comment;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Event;
@@ -11,9 +13,11 @@ use Tests\TestCase;
 
 class CommentListenerTest extends TestCase
 {
+    use WithFaker;
+
 
     /**
-     * A basic feature test example.
+     * Testing CommentWritten Event and its Listener are connected to each other or not.
      */
     public function test_listener_connected_to_event(): void
     {
@@ -23,5 +27,36 @@ class CommentListenerTest extends TestCase
             CommentWritten::class,
             CommentListener::class
         );
+    }
+
+    /**
+     * Testing Generating 2 Comment and then faking CommentWritten event and getting the response.
+     * @return void
+     */
+    public function test_check_unlocker_logic(){
+
+        $comment = $this->generating_data(2);
+
+        event( new CommentWritten($comment));
+
+        $response = $this->get('/users/' . $comment->user_id . '/achievements/');
+        $response->assertStatus( 200);
+        $response->dd();
+
+
+    }
+
+    protected function generating_data( $num_comment){
+        $this->setUpFaker();
+        $user = User::factory()->create();
+
+        while( $num_comment >= 0){
+           Comment::create([
+               "body" => $this->faker()->text,
+               "user_id" => $user->id,
+           ]);
+           $num_comment--;
+        }
+        return Comment::first();
     }
 }
